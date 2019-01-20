dac35d4c7ea03a31cb48979180ef3cbe669e892a
Binod <chaku.lama@gmail.com>
2019-01-20 13:20:25 +0100

    Multiplication of two numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ProyectoTutorial';
   numero:number;
+  numero1:number;
+  numero2:number;
   resultado:number;
+  resultadoMulti:number;
 
   Duplicar(){
     console.log("Voy a duplicar "+ this.numero);
     this.resultado=this.numero*2;
-    console.log("Resultado es"+this.resultado);
+    console.log("Resultado es "+this.resultado);
+  }
+
+  Multiply(){
+    console.log("Multiplication of "+ this.numero + " and "+this.numero );
+    this.resultadoMulti=this.numero1*this.numero2;
+    console.log("Resultado es "+this.resultadoMulti);
   }
 }
