d94660019d1851f841fbe6b16e66839a2def522e
Binod <chaku.lama@gmail.com>
2019-01-31 22:03:12 +0100

    Angular Material Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule}from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatBadgeModule} from '@angular/material/badge';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { PersonaComponent } from './persona/persona.component';
 import { ProfesorComponent } from './profesor/profesor.component';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     PersonaComponent,
-    ProfesorComponent
+    ProfesorComponent,
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
